30b5e00994ff054970070eac3ba5bd1711c83f2c
Nick Brown <jellatin@gmail.com>
2013-04-01 22:11:34 -0700

    undone

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130402050946) do
+
+  create_table "first_names", :force => true do |t|
+    t.string   "last_name"
+    t.integer  "phone"
+    t.string   "address1"
+    t.string   "address2"
+    t.integer  "zip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
